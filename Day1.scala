75dad766f5205cc4fb06224bce15e4b368f4907f
madeleineberild <madeleine.berild@hotmail.com>
2020-12-02 20:19:45 +0100

    Solve part 2

--- Day1.scala
+++ Day1.scala
@@ -16,6 +16,10 @@ class Day1 {
         x + y == 2020
     }
 
+    def checkSum(x: Int, y: Int, z: Int): Boolean = {
+        x + y + z == 2020
+    }
+
     def findPair: (Int, Int) = {
         var result = (0,0)
         for(x <- integers) {
@@ -28,11 +32,26 @@ class Day1 {
         result
     }
 
+    def findThrees: (Int, Int, Int) = {
+        var result = (0, 0, 0)
+        for(x <- integers; y <- integers; z <- integers) {
+            if(checkSum(x, y, z)) result = (x, y, z)
+        }
+        result
+    }
+
     def run() = {
+        //part 1
         fromFile("day1.txt")
         val x = findPair._1
         val y = findPair._2
-        println(s"The numbers are $x and $y. The sum is ${x * y}")
+        println(s"The numbers are $x and $y. The product is ${x * y}")
+
+        //part 2
+        val a = findThrees._1
+        val b = findThrees._2
+        val c = findThrees._3
+        println(s"The numbers are $a, $b and $c. The product is ${a*b*c}.")
     }
 
 }
