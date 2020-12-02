03068f286e49f07bfcaed137f3d92ec54bf9b749
madeleineberild <madeleine.berild@hotmail.com>
2020-12-02 20:12:39 +0100

    Solve part 1

--- Day1.scala
+++ Day1.scala
@@ -12,9 +12,27 @@ class Day1 {
         }
     }
 
+    def checkSum(x: Int, y: Int): Boolean = {
+        x + y == 2020
+    }
+
+    def findPair: (Int, Int) = {
+        var result = (0,0)
+        for(x <- integers) {
+            for(y <- integers) {
+                if(checkSum(x, y)) {
+                    result = (x, y)
+                }
+            }
+        }
+        result
+    }
+
     def run() = {
         fromFile("day1.txt")
-        
+        val x = findPair._1
+        val y = findPair._2
+        println(s"The numbers are $x and $y. The sum is ${x * y}")
     }
 
 }
--- Main.scala
+++ Main.scala
@@ -1,9 +1,11 @@
 package adventofcode
 
 object Main {
+    val day1 = new Day1
 
     def main(args: Array[String]): Unit = {
         println("Hello advent of code!")
+        day1.run
     }
 
 }
