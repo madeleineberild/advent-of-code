b847d7392698df458ff3233a87f48e0a75adac1c
madeleineberild <madeleine.berild@hotmail.com>
2020-12-02 20:24:54 +0100

    Setup day 2

--- Main.scala
+++ Main.scala
@@ -2,10 +2,12 @@ package adventofcode
 
 object Main {
     val day1 = new Day1
+    val day2 = new Day2
 
     def main(args: Array[String]): Unit = {
         println("Hello advent of code!")
-        day1.run
+        val today = day2
+        today.run
     }
 
 }
