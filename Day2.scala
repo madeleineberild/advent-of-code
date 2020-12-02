42ae15963b0d4cf520b974f2a8c6f45528f1b94e
madeleineberild <madeleine.berild@hotmail.com>
2020-12-02 21:19:18 +0100

    Solve part 1

--- Day2.scala
+++ Day2.scala
@@ -1,5 +1,35 @@
 package adventofcode
+import scala.collection.mutable.ArrayBuffer
+import scala.io.Source
 
 class Day2 {
-    
+    val input = new ArrayBuffer[String]()
+
+    def fromFile(fileName: String) = {
+        for(line <- Source.fromFile(fileName).getLines) {
+            input.append(line)
+        }
+    }
+
+    def isValid(x: String): Boolean = {
+        val rangeString = x.split(':')(0)
+        val password = x.split(':')(1).trim
+        val low = rangeString.split('-')(0).toInt
+        val letter = rangeString.split('-').mkString.trim.last
+        val high = rangeString.split('-')(1).dropRight(1).trim.toInt
+        val range = low to high
+        val count = password.filter(x => x == letter).size
+        range.contains(count)
+    }
+
+    def numberOfValid: Int = {
+        println(input.size)
+        val partial = input.filter(x => isValid(x))
+        partial.size
+    }
+
+    def run = {
+        fromFile("day2.txt")
+        println(s"Only $numberOfValid passwords are valid in this database")
+    }
 }
