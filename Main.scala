694dc5d16489ba6fad4cfd0adf7463ba11b0b04e
madeleineberild <madeleine.berild@hotmail.com>
2020-12-04 13:15:50 +0100

    Setup day 4

--- Main.scala
+++ Main.scala
@@ -4,10 +4,11 @@ object Main {
     val day1 = new Day1
     val day2 = new Day2
     val day3 = new Day3
+    val day4 = new Day4
 
     def main(args: Array[String]): Unit = {
         println("Hello advent of code!")
-        val today = day3
+        val today = day4
         today.run
     }
 
