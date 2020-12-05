1c6ba2b55c7b45895cfa8784842a2dfc67186241
madeleineberild <madeleine.berild@hotmail.com>
2020-12-05 08:52:34 +0100

    Add Boarding Pass

--- Day5.scala
+++ Day5.scala
@@ -1,9 +1,36 @@
 package adventofcode
 
+import scala.io.Source
+import scala.collection.ArrayBuffer
+
 class Day5 {
+    val input = new ArrayBuffer[String]()
+
+    def fromFile(fileName: String) = {
+        for(line <- Source.fromFile(fileName).getLines) {
+            input.append(line)
+        }
+    }
 
     def run = {
         println("Hello day 5")
+        fromFile
+        println(input.toString)
     }
 
+}
+
+case class BoardingPass(input: String) {
+    val seatRow = row
+    val seatCol = col
+
+    def seatID = seatRow * 8 + seatCol
+
+    def row: Int = {
+        
+    }
+
+    def col: Int = {
+
+    }
 }
